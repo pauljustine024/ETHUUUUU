098e485a1eecc2ed05bd592d38ba09965381d02f
pauljustine024 <128199663+pauljustine024@users.noreply.github.com>
2023-04-23 18:49:47 +0800

    Create MyTokens.sol

---
+++ MyTokens.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract NTC {
+    
+    string public tokenName = "NTC Token";
+    string public tokenSymbol = "NTC";
+    uint public totalSupply = 0;
+    
+    mapping(address => uint) public balances;
+
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    function burn(address _address, uint _value) public {
+        if (balances[_address] >= _value){
+        }
+        totalSupply -= _value;
+        balances[_address] -= _value;
+    }
+}
